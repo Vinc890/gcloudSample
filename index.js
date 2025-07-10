33a25b0f47d7129f7781a2c8c9ae5c9cbcaaf13f
Vincent Varghese <vinxent890@gmail.com>
2025-07-10 17:31:41 +0530

    spawn in

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
 app.get("/", (req, res) => {
 
