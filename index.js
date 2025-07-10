e6cf1167a0179b86f9edecf8653731ea015360ae
Vincent Varghese <vinxent890@gmail.com>
2025-07-10 16:53:53 +0530

    command

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
+const { spawn } = require("child_process");
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  const command = "ffmpeg --help";
+  const child = spawn(command, []);
+  child.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  child.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  child.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
+  child.on("error", (err) => {
+    console.error("Failed to start child process.", err);
+  });
+  res.send("Hello World");
 });
 
 app.listen(PORT, () => {
