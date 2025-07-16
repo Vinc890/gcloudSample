616ce1a2dae46df5b107e1bf80282ceea61d6b9f
Vincent Varghese <vinxent890@gmail.com>
2025-07-16 14:16:09 +0530

    Creds

--- index.js
+++ index.js
@@ -30,22 +30,7 @@ const ttsClient = new TextToSpeechClient();
 
 async function getAccessToken() {
   const auth = new GoogleAuth({
-    keyFile: {
-      type: "service_account",
-      project_id: "contactaiassessments",
-      private_key_id: "238a6770ff60a2556e9aa80e4bf25049f7d00a01",
-      private_key:
-        "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCb1dSmL7txWvqW\n5C1rkrw2dqaGciTLdgj4YjJbzC0ZSU0UpbbqXkx51SyXKhiArB4H6TfK3sImDJqL\n/kZl8MvWlWCegoXOeiXtwhLxsD6xOCMwcIbSaeggCKH9q3M8MjR0+4G6w6pKykkU\nXLeToLjNzQOGe5rf9Igz4Z5CEcz8Ju+ZUgiSrVYbJTz0eDjvo57Dtfu6ro/AYS/5\nXnFKRts+4VerM3RCZsFRLhP7pjbL0ywYUW3Tl1k6RyOvA9cxoLXVGoX3Dz7nTCtF\nyYG4iKZ1wJLO6xngspFYGKYKWpnu8A8y3jPeSzP6INdPKkmSdYPaiZUhpbgy8mIb\nMjlbt9ZpAgMBAAECggEADquDEL/2k9W+OF0zn2hZWoEx7P8q0pKChqtr1TNz3WkG\nnhZ5kTeeWGvGflaCpv5M926vh2QP/9f45ovh4a/Y6JL4XQOpiAX8sxStht4SEMnZ\nmjJFpuI8bWOSqFgvCXAqewbAMC5CRjcjyQxvZbDgJNOTbOIO8t1IwyWOqeaWFviH\nqKRjb+H63TQ/tj4OX2DxElCa9js166/FOrBPR7Fk4GS2CG0w+d2Lhk1FFf+v42QE\nLLbxtUTKZg68HkrUQ983qzMi0t8ddH6Jf459HGCof8PmZr1SVFjRWbyiaLKyTu7p\n1apyJpchmMuE4w0ypl7WWucAto3yQZ9kycbW/TD1nwKBgQDXRZb5S4QeWGZmKe6+\nZJl6h/6ijGYNgqjnHLoeMdF9TS5Qxjhvlp8ucpi6eotFEvRcR5QigtSLIX+lUKMj\nc2UyQVcY2bFfys5dJeFad5trg9WIeBvpa6BvyjqYu1F8OuNSpaOXZ6Nk+vSC3rLX\n/sQv0qZvwOfUmMOoXbF7aBWntwKBgQC5UYJAIF39DyceOWYpgdXm73Erde/ezHk8\nv0hvugKHOByc1DUdUP0lyXNULo+8K5tBkBYN1SYoFXRE6yEMGOC48Ud7OPaJa7Rk\nNMdy4f85JKY3BdgrgzGOD1SoZrQtN4QFnr+rthFsyFAMjBkuP9U7ehX+HyyFaDI8\nS6MGsK8y3wKBgQDB948oQzXhTc++YCwhW227LUxv1EekBsX/sC+3QzY6S8/esixp\nx3LYnCMna4GPlJufhlNgoTe3wVBNeZH1QGW/WYaL+qLK6Gb3IUmjhUACKUC+/VJR\nCUv/Tl1r/uRWJo1ri5oSsyxTsZedT+IfowvM92ZGLa/2LEunqfxgcJGKkwKBgD1V\nJqCGldS9ARtVr+Qo3lxR/sh9feflEHL0c8rWayPJhF67NOEA/udUpuDDkDqczAOE\n5meplblKcHKmxwcz7JwI7rlvfti4VrmbZi81cLy+zmwDeSndf7ceh8w8QYF9kCo0\nAgeYeGfiW+vrKiJOagoHO+Qg+SEl/QpLlicOrs1NAoGAXjx5voRw2AiuGY2tohtM\nIG0O6ohFhHUFDyg0UZS1o4OMfpA0vMx/SXhLG09fnXGtAHD6cLqoHVUkTOxjNzDY\nLLLjL35+y0ixtqRXR9Rndh0hGaDroy0PT8r0piLmKHv2TxRkHMZSiUMvHOpoUibT\nyCEd9D9ogCvkpvghVSZataM=\n-----END PRIVATE KEY-----\n",
-      client_email:
-        "service-account-test@contactaiassessments.iam.gserviceaccount.com",
-      client_id: "113213039598146082674",
-      auth_uri: "https://accounts.google.com/o/oauth2/auth",
-      token_uri: "https://oauth2.googleapis.com/token",
-      auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-      client_x509_cert_url:
-        "https://www.googleapis.com/robot/v1/metadata/x509/service-account-test%40contactaiassessments.iam.gserviceaccount.com",
-      universe_domain: "googleapis.com",
-    },
+    keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
     scopes: ["https://www.googleapis.com/auth/cloud-platform"],
   });
   const client = await auth.getClient();
